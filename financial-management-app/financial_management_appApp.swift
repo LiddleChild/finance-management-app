7ccd3e5d2b0c9c2d9aebef65d22f440e0280fb2f
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-06-18 15:57:54 +0700

    Initial Commit

---
+++ financial-management-app/financial_management_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  financial_management_appApp.swift
+//  financial-management-app
+//
+//  Created by Thanapat Ussawanarong on 18/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct financial_management_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
