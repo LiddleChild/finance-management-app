b8a8f2d8d23e234c29ced6d54c75d81490fd61dd
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-06-18 23:28:16 +0700

    feat: add custom textfield

--- financial-management-app/ContentView.swift
+++ financial-management-app/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomTextField: View {
+    var label: String
+    var placeholder: String
+    
+    @State private var isFocus: Bool = false
+    @State private var text: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("", text: $text) { (status) in
+                //
+                isFocus.toggle()
+            } onCommit: {
+                //
+            }
+            .autocapitalization(.none)
+            .foregroundColor(Color("color-5"))
+            .padding()
+            .background(
+                ZStack(alignment: .leading) {
+                    // Border
+                    RoundedRectangle(cornerRadius: 12)
+                        .strokeBorder(Color("color-\(isFocus ? 4 : 3)"))
+                    
+                    // Label
+                    Text(label)
+                        .font(.system(size: 20, weight: .regular))
+                        .padding(.horizontal, 4)
+                        .foregroundColor(Color("color-5"))
+                        .background(Color("color-1"))
+                        .offset(x: 16, y: -28)
+
+                    // Placeholder
+                    if !isFocus || (isFocus && text == "") {
+                        Text(verbatim: placeholder)
+                            .font(.system(size: 16, weight: .light))
+                            .foregroundColor(Color("color-3"))
+                            .padding(16)
+                    }
+                }
+            )
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color("color-1"))
+                .ignoresSafeArea()
+            
+            VStack {
+                CustomTextField(label: "Email", placeholder: "example@mail.com")
+                CustomTextField(label: "Password", placeholder: "********")
+            }
+        }
     }
 }
 
